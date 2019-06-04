bfaccaae2144508ce4a9595319bc9d8da7265421
Viktor Rozenko <sharp.vik@gmail.com>
2019-06-04 19:56:02 +0100

    actually it is bytes writer

--- Package/Bytes/writer.go
+++ Package/Bytes/writer.go
@@ -1,5 +1,33 @@
 package bytes
 
-func Write(byteCode []*Instruction) {
-    //
+import (
+    "bufio"
+    "errors"
+    "os"
+)
+
+func Write(assemblyCode []*Instruction, filename string) error {
+    // Convert []*bytes.Instruction to []byte
+    var byteCode []byte
+    for _, i := range assemblyCode {
+        byteCode = append(byteCode, i.Opcode)
+        for _, b := range i.Operand {
+            byteCode = append(byteCode, b)
+        }
+    }
+    
+    // Create file
+    file, err := os.Create(filename)
+    if err != nil {
+        return err
+    }
+    
+    // Create bufio.Writer and write to file
+    wrtr := bufio.NewWriter(file)
+    n, err := wrtr.Write(byteCode)
+    if err != nil {
+        return err
+    }
+    
+    return nil
 }
